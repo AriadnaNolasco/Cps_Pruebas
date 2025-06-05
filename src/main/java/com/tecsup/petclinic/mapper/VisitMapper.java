658f6885f8553bb77f51f0e76fd2f485c896cc22
Anjeli Verastigue <anjeli.verastigue@tecsup.edu.pe>
2025-06-05 16:31:20 -0500

    Mapper

--- src/main/java/com/tecsup/petclinic/mapper/VisitMapper.java
+++ src/main/java/com/tecsup/petclinic/mapper/VisitMapper.java
@@ -12,40 +12,39 @@ import java.text.SimpleDateFormat;
 import java.sql.Date;
 import java.util.List;
 
-@Mapper(componentModel = "spring", nullValueMappingStrategy =  NullValueMappingStrategy.RETURN_DEFAULT)
-public class VisitMapper {
+@Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface VisitMapper {
 
     VisitMapper INSTANCE = Mappers.getMapper(VisitMapper.class);
 
-    @Mapping(source = "visitDate", target = "visitDate")
+    @Mapping(source = "visitDate", target = "visitDate", dateFormat = "yyyy-MM-dd")
     Visit toVisit(VisitDTO visitDTO);
 
-    default Date stringToDate(String dateStr) {
-        Date date = null;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            java.util.Date parsed = dateFormat.parse(dateStr);
-            date = new Date(parsed.getTime());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
-    }
-
-    @Mapping(source = "visitDate", target = "visitDate")
+    @Mapping(source = "visitDate", target = "visitDate", dateFormat = "yyyy-MM-dd")
     VisitDTO toVisitDTO(Visit visit);
+
+    List<Visit> toVisitList(List<VisitDTO> visitDTOList);
+
+    List<VisitDTO> toVisitDTOList(List<Visit> visitList);
+
+    default Date stringToDate(String dateStr) {
+        if (dateStr != null && !dateStr.isEmpty()) {
+            try {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                java.util.Date parsed = dateFormat.parse(dateStr);
+                return new Date(parsed.getTime());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 
     default String dateToString(Date date) {
         if (date != null) {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             return dateFormat.format(date);
-        } else {
-            return "";
         }
+        return null;
     }
-
-    List<VisitDTO> toVisitDTOList(List<Visit> visitList);
-
-    List<Visit> toVisitList(List<VisitDTO> visitDTOList);
 }
-
