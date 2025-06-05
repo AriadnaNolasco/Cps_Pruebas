f82bb5d0b32cf2f4c7cc6565f7aa6dcd93a4dffe
Anjeli Verastigue <anjeli.verastigue@tecsup.edu.pe>
2025-06-05 16:45:01 -0500

    Create

--- src/test/java/com/tecsup/petclinic/web/VisitControllerMockitoTest.java
+++ src/test/java/com/tecsup/petclinic/web/VisitControllerMockitoTest.java
@@ -1,4 +1,69 @@
 package com.tecsup.petclinic.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tecsup.petclinic.dtos.VisitDTO;
+import com.tecsup.petclinic.entities.Visit;
+import com.tecsup.petclinic.services.VisitService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Slf4j
 public class VisitControllerMockitoTest {
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private VisitService visitService;
+
+    @BeforeEach
+    void setUp() {
+
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testCreateVisit() throws Exception {
+
+        String description = "Routine Check";
+
+        VisitDTO newVisitDTO = new VisitDTO();
+        newVisitDTO.setDescription(description);
+
+        Visit newVisit = new Visit();
+        newVisit.setDescription(description);
+
+        Mockito.when(visitService.create(newVisit))
+                .thenReturn(newVisit);
+
+        mockMvc.perform(post("/visits")
+                        .content(om.writeValueAsString(newVisitDTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.description", is(description)));
+    }
 }
